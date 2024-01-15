b10be123cde81c0dc6b5602b0f0fe2640356def1
ibtasam116 <ibtasam116@gmail.com>
2024-01-15 14:24:37 +0500

    Fixing Errors

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Nunito({ subsets: ["latin"], variable: "--Nunito" });
 
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+
 export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import weatherSlice from "./slices/weatherSlice";
-// import searchSlice from "./slices/searchSlice";
 
 export const store = configureStore({
   reducer: {
     weather: weatherSlice,
-    // search: searchSlice,
   },
 });
